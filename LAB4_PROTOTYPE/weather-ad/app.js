18081ff2d26850c9652ba64da409bf4e5860a5c0
Bryan Van Mechelen <bryan.van.mechelen@hotmail.com>
2020-03-25 20:26:10 +0100

    Edited getWeather function in JS to also display text in HTML

--- LAB4_PROTOTYPE/weather-ad/app.js
+++ LAB4_PROTOTYPE/weather-ad/app.js
@@ -14,21 +14,49 @@ class App{
         this.lat = result.coords.latitude;
         this.lng = result.coords.longitude;
         this.getWeather();
+        this.getGame();
     }
 
     getWeather(){
          
-        let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/de41b82410acc0ea1e904b4c96eb9b1e/${this.lat},${this.lng}?units=si`;
-        fetch(url)
-            .then(response => {
-                return response.json();
+        let temperature = localStorage.getItem("temperature");
+        setTimeout(() => {localStorage.removeItem("temperature");},1000*60*60);
+        if(temperature == null || temperature == "null"){
+            console.log("nodata");
+            let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/de41b82410acc0ea1e904b4c96eb9b1e/${this.lat},${this.lng}?units=si`;
+            fetch(url)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data =>{
+                    temperature = localStorage.setItem("temperature",(data.currently.temperature));
+                    document.querySelector("#weather").innerHTML = "Het is " + data.currently.temperature + " graden.";
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }else{
+            console.log("data from storage");
+            document.querySelector("#weather").innerHTML = "Het is " + localStorage.getItem("temperature") + " graden."
+        }
+    }
+    getGame(){
+        let graden = document.querySelector("#weather").innerHTML;
+        if(graden <= 20){
+            fetch("https://rawg-video-games-database.p.rapidapi.com/games", {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+                    "x-rapidapi-key": "9e620c3e1amsh83cc64223ab9646p1f70e9jsn4250c13eed9c"
+                }
             })
-            .then(data =>{
-                document.querySelector("#weather").innerHTML = data.currently.temperature;
+            .then(response => {
+                console.log(response);
             })
             .catch(err => {
                 console.log(err);
             });
+        }
     }
 
     errorLocation(result){
